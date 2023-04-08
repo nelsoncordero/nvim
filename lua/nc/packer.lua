4efba208034c1dad6fa91df54706607c06ddcfef
Nelson Cordero <nelsondavid.cordero@gmail.com>
2023-04-08 14:23:41 -0400

    Packer format

--- lua/nc/packer.lua
+++ lua/nc/packer.lua
@@ -6,11 +6,9 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
+
+  use 'nvim-lua/plenary.nvim'
+  use { 'nvim-telescope/telescope.nvim', tag = '0.1.1' }
 
   use 'nvim-tree/nvim-web-devicons'
   use 'nvim-tree/nvim-tree.lua'
