e7f4f58f2af6c2c7beee55498f244fffe6db4809
Nelson Cordero <nelsondavid.cordero@gmail.com>
2023-04-08 22:59:12 -0400

    Add LSP

--- lua/nc/packer.lua
+++ lua/nc/packer.lua
@@ -16,8 +16,28 @@ return require('packer').startup(function(use)
   use 'vim-airline/vim-airline'
   use 'doums/darcula'
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use 'mbbill/undotree' 
+  use 'mbbill/undotree'
   use 'tpope/vim-commentary'
   use 'akinsho/toggleterm.nvim'
   use 'lewis6991/gitsigns.nvim'
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
 end)
