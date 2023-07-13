50ef61157b4f5c2d31f00d2b44064f0451f4c9d2
Nelson Cordero <nelsondavid.cordero@gmail.com>
2023-07-13 13:09:29 -0400

    Add tmux-navigator.

--- lua/nc/packer.lua
+++ lua/nc/packer.lua
@@ -7,6 +7,8 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  use { 'christoomey/vim-tmux-navigator', lazy = false }
+
   use 'nvim-lua/plenary.nvim'
   use 'sheerun/vim-polyglot'
   use { 'nvim-telescope/telescope.nvim', tag = '0.1.1' }
