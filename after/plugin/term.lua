344658c46a6e8b6987426b0533a4baf232370023
Nelson Cordero <nelsondavid.cordero@gmail.com>
2024-03-19 18:33:43 -0400

    Add standard term

--- after/plugin/term.lua
+++ after/plugin/term.lua
@@ -63,3 +63,27 @@ end
 
 vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 
+
+local standar = Terminal:new({
+  -- cmd = "lazygit",
+  dir = "git_dir",
+  direction = "horizontal",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
+
+function _stardar_toggle()
+  standar:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>z", "<cmd>lua _stardar_toggle()<CR>", {noremap = true, silent = true})
