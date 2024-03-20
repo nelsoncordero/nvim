6f167ee54150b1e81bc9387bff338923cebbe7a2
Nelson Cordero <nelsondavid.cordero@gmail.com>
2024-03-20 19:43:54 -0400

    Add autoparis for %

--- after/plugin/autopairs.lua
+++ after/plugin/autopairs.lua
@@ -22,3 +22,12 @@ npairs.setup({
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
+local Rule = require('nvim-autopairs.rule')
+local cond = require('nvim-autopairs.conds')
+
+npairs.add_rules({
+  Rule("%", "%")
+    :with_pair(cond.before_text("{"))
+  }
+)
