2544dedd90c26a895283b2a7ae1fa5e656142124
Nelson Cordero <nelsondavid.cordero@gmail.com>
2024-03-20 19:43:44 -0400

    Add Telescope buffers shortcut

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,6 +3,7 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fW', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_status, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
