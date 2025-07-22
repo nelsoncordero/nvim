c5b9d10d9eb7ca96a7fd7f325791f97fbcf0b51d
Nelson Cordero <nelsondavid.cordero@gmail.com>
2025-07-22 19:09:42 -0400

    Add quit window map

--- lua/nc/remap.lua
+++ lua/nc/remap.lua
@@ -25,6 +25,7 @@ vim.keymap.set("i", "<Leader>v", vim.cmd.vsplit)
 vim.keymap.set("n", "<Leader>w", "<C-w>")
 vim.keymap.set("n", "<Leader>l", "<C-w>l")
 vim.keymap.set("n", "<Leader>h", "<C-w>h")
+vim.keymap.set("n", "<Leader>q", "<C-w>q")
 
 -- Resize window
 vim.keymap.set("i", "<A-l>", "<C-w>5>")
