0b9dcad24ecd86c562bbf0e876cb7d182017ebeb
Nelson Cordero <nelsondavid.cordero@gmail.com>
2026-08-03 21:08:02 -0400

    Improve git_status and remove commented lines

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -47,23 +47,6 @@ telescope.setup{
 }
 telescope.load_extension("live_grep_args")
 
-local delta = previewers.new_termopen_previewer {
-  get_command = function(entry)
-    return { "git", "-c", "core.pager=delta", "-c", "delta.line-numbers=true", "-c", "delta.pager=less -R", "show", entry.value, '--', entry.current_file }
-  end
-}
-
-local my_git_bcommits = function(opts)
-  opts = opts or {}
-  opts.previewer = {
-    delta,
-    previewers.git_commit_message.new(opts),
-    previewers.git_commit_diff_as_was.new(opts),
-  }
-
-  builtin.git_bcommits(opts)
-end
-
 local ignored_dirs = {
   '%.git/',
   '%.idea/',
@@ -77,47 +60,22 @@ end)
 vim.keymap.set("n", "<leader>fa", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fW', builtin.grep_string, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_status, {})
 vim.keymap.set('n', '<leader>b', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fs', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
-vim.keymap.set('n', '<leader>fG', function() my_git_bcommits(); end)
 
 
 vim.cmd "autocmd User TelescopePreviewerLoaded setlocal number"
 
-local previewers = require('telescope.previewers')
-
--- local delta = previewers.new_termopen_previewer {
---   get_command = function(entry)
---     -- note we can't use pipes
---     -- this command is for git_commits and git_bcommits
---     return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!' }
-
---     -- this is for status
---     -- You can get the AM things in entry.status. So we are displaying file if entry.status == '??' or 'A '
---     -- just do an if and return a different command
---     -- return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value }
---   end
--- }
-
-
--- local my_git_bcommits = function(opts)
---   opts = opts or {}
---   opts.previewer = delta
-
---   builtin.git_bcommits(opts)
--- end
-
 local delta = previewers.new_termopen_previewer {
   get_command = function(entry)
     -- return { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff', entry.value .. '^!', '--', entry.current_file }
-    return { "git", "-c", "core.pager=delta", "-c", "delta.line-numbers=true", "-c", "delta.pager=less -R", "show", entry.value, '--', entry.current_file }
+    return { "git", "-c", "core.pager=delta", "-c", "delta.pager=less -R", "show", entry.value, '--', entry.current_file }
   end
 }
 
-local my_git_bcommits = function(opts)
+local delta_git_bcommits = function(opts)
   opts = opts or {}
   opts.previewer = {
     delta,
@@ -128,4 +86,31 @@ local my_git_bcommits = function(opts)
   builtin.git_bcommits(opts)
 end
 
-vim.keymap.set('n', '<leader>fG', function() my_git_bcommits(); end)
+vim.keymap.set('n', '<leader>fG', function() delta_git_bcommits(); end)
+
+
+local delta_previewer = previewers.new_termopen_previewer {
+  get_command = function(entry)
+    -- Handle git_commits, git_bcommits, and git_status
+    local command = { 'git', '-c', 'core.pager=delta', '-c', 'delta.side-by-side=false', 'diff' }
+    if entry.status and entry.status == '??' then
+      return { 'cat', entry.value } -- Show untracked files
+    end
+    table.insert(command, entry.value .. (entry.status and '' or '^!'))
+    return command
+  end
+}
+
+-- Apply custom previewer to pickers
+local function git_with_delta(picker)
+  return function(opts)
+    opts = opts or {}
+    opts.previewer = delta_previewer
+    picker(opts)
+  end
+end
+
+-- Example Keymaps
+-- vim.keymap.set('n', '<leader>gc', git_with_delta(builtin.git_commits), { desc = "Git Commits (Delta)" })
+vim.keymap.set('n', '<leader>fg', git_with_delta(builtin.git_status), { desc = "Git Status (Delta)" })
+
