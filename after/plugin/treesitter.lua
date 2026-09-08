94f053c333770d2d3a28f71ef0d258398c017c22
Nelson Cordero <nelsondavid.cordero@gmail.com>
2026-09-08 14:53:42 -0400

    Update tree-sitter installs

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "c", "lua", "vim", "vimdoc", "query", "python" },
+  ensure_installed = { "javascript", "python" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -12,6 +12,6 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = false,
-    disable = { 'markdown' },
+    disable = { 'markdown', 'html' },
   },
 }
