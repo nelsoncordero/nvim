b21b29b490130ca419afce7a3f70f318406092fc
Nelson Cordero <nelsondavid.cordero@gmail.com>
2026-09-20 22:23:16 -0400

    Updates bindings

--- lua/nc/plugins.lua
+++ lua/nc/plugins.lua
@@ -74,8 +74,8 @@ local plugins = {
     event = "VeryLazy",
     opts = {},
     keys = {
-      { "/", mode = { "n", "x", "o" }, function() vim.go.ignorecase = true require("flash").jump() end, desc = "Flash" },
-      { "?", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "s", mode = { "n", "x", "o" }, function() vim.go.ignorecase = true require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
       { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
       { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
